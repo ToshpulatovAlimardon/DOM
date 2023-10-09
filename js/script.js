1c1dc8b3793b6d61c1604c5e626084305b2ad0f7
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-09 16:59:56 +0500

    changed button for forEach

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const hearts = document.querySelectorAll('.heart');
+const buttons = document.querySelectorAll(".btn");
 
-hearts.forEach(item => {
-    item.style.cssText = 'box-shadow: 0 0 5px 5px purple; background-color: #0f0;'
-})
+buttons.forEach((item) => {
+    item.style.cssText =
+        "background: #0f0; color: #000; font-weight: bold; font-size: 24px; width: 150px; height: 150px";
+});
