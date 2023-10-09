b1139cfdec5afa71742f09b3e41ca62dfd6df358
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-09 16:53:23 +0500

    changed heart for forEach

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 "use strict";
 
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    item.style.cssText = 'box-shadow: 0 0 5px 5px purple; background-color: #0f0;'
+})
